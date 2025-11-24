f66c244450ef8078b6a37fc4d7c43eee84c90e89
Loner David <davidyang19995@gmail.com>
2025-11-24 18:15:27 +0800

    App project added

---
+++ NMLab_Final_App/NMLab_Final_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NMLab_Final_App
+//
+//  Created by Haruaki on 2025/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
